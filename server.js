0c6b4ade4474774c13dcf9753d5e4da50c0b1f13
sukhmansingh295 <sukhman295r@gmail.com>
2026-02-19 23:22:10 +0530

    quiz system 2331

--- server.js
+++ server.js
@@ -73,50 +73,105 @@ function tryMatch() {
 function startQuiz(p1, p2) {
 
   let questionIndex = 0;
+  const TOTAL_QUESTIONS = quizQuestions.length;
 
   function sendQuestion() {
 
     if (!p1.connected || !p2.connected) return;
 
-    if (questionIndex >= quizQuestions.length) {
-
-      p1.emit("quiz-end", { score: p1.score });
-      p2.emit("quiz-end", { score: p2.score });
-
+    if (questionIndex >= TOTAL_QUESTIONS) {
+      finishQuiz();
       return;
     }
 
     const question = quizQuestions[questionIndex];
 
-    p1.emit("quiz-question", question);
-    p2.emit("quiz-question", question);
+    let answers = {
+      [p1.id]: null,
+      [p2.id]: null
+    };
 
-    let answered1 = false;
-    let answered2 = false;
+    const payload = {
+      id: questionIndex,
+      question: question.question,
+      options: question.options,
+      duration: 10
+    };
 
-    p1.once("quiz-answer", (index) => {
-      if (index === question.answer) p1.score++;
-      answered1 = true;
-    });
+    p1.emit("quiz-question", payload);
+    p2.emit("quiz-question", payload);
 
-    p2.once("quiz-answer", (index) => {
-      if (index === question.answer) p2.score++;
-      answered2 = true;
-    });
+    function handleAnswer(player, index) {
 
-    setTimeout(() => {
+      if (answers[player.id] !== null) return;
 
-      p1.emit("quiz-score", { score: p1.score });
-      p2.emit("quiz-score", { score: p2.score });
+      answers[player.id] = index;
+
+      if (index === question.answer) {
+        player.score += 5;
+      }
+
+      checkIfBothAnswered();
+    }
+
+    function checkIfBothAnswered() {
+      if (
+        answers[p1.id] !== null &&
+        answers[p2.id] !== null
+      ) {
+        closeQuestion();
+      }
+    }
+
+    p1.once("quiz-answer", (index) => handleAnswer(p1, index));
+    p2.once("quiz-answer", (index) => handleAnswer(p2, index));
+
+    function closeQuestion() {
+
+      p1.removeAllListeners("quiz-answer");
+      p2.removeAllListeners("quiz-answer");
+
+      p1.emit("quiz-result", {
+        correctAnswer: question.answer,
+        score: p1.score
+      });
+
+      p2.emit("quiz-result", {
+        correctAnswer: question.answer,
+        score: p2.score
+      });
 
       questionIndex++;
-      sendQuestion();
 
-    }, 10000);
+      setTimeout(sendQuestion, 3000);
+    }
+
+    setTimeout(closeQuestion, 10000);
+  }
+
+  function finishQuiz() {
+
+    let winner = null;
+
+    if (p1.score > p2.score) winner = p1.id;
+    else if (p2.score > p1.score) winner = p2.id;
+
+    p1.emit("quiz-end", {
+      yourScore: p1.score,
+      opponentScore: p2.score,
+      winner: winner
+    });
+
+    p2.emit("quiz-end", {
+      yourScore: p2.score,
+      opponentScore: p1.score,
+      winner: winner
+    });
   }
 
   sendQuestion();
 }
+
 
 /* ---------------- SOCKET EVENTS ---------------- */
 
